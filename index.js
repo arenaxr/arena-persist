af7f493f3114cfa8b7b1cfc4da23a2bbf33263de
Ivan <r33t@cmu.edu>
2019-10-30 16:28:08 -0400

    Log realm and scene from topic to be later queryable
    Conceivably, this means that a persistence service is tracking
    more than it's own (default) realm.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const mqtt = require('async-mqtt');
 
 
-mongoose.connect(config.mongodb.uri, {useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect(config.mongodb.uri, {useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Mongodb connection error:'));
 db.once('open', () => {
@@ -12,76 +12,84 @@ db.once('open', () => {
 });
 
 const arenaSchema = new mongoose.Schema({
-    object_id: String,
+    object_id:  { type: String, index: true, unique: true },
     attributes: Map,
     createdAt: Date,
     lastUpdated: Date,
+    realm:  { type: String, index: true },
+    scene:  { type: String, index: true },
 });
 
 const ArenaObject = mongoose.model('ArenaObject', arenaSchema);
 
 async function runMQTT() {
     const mqttClient = await mqtt.connectAsync(config.mqtt.uri);
-    const MQTT_TOPIC = config.mqtt.topic;
+    const SCENE_TOPICS = config.mqtt.topic_realm + "/s/#";
     console.log("Connected to MQTT");
-    try{
-        await mqttClient.subscribe(MQTT_TOPIC).then(() => {
-            mqttClient.publish(config.mqtt.statusTopic, 'Persistence service connected');
+    try {
+        await mqttClient.subscribe(SCENE_TOPICS).then(() => {
+            mqttClient.publish(config.mqtt.statusTopic, 'Persistence service connected: ' + config.mqtt.topic_realm);
         });
         mqttClient.on('message', async (topic, message) => {
-            let msgJSON = undefined;
+            let topicSplit = topic.split("/");
+            /*
+            Topic tokens by forward slash:
+            - 0: realm
+            - 1: type [s, n, r, topology, flows]
+            - 2: scene_id
+            */
             try {
-                msgJSON = JSON.parse(message.toString())
+                let msgJSON = JSON.parse(message.toString())
             } catch(e) {
-                // pass
+                return;
             }
-            if (msgJSON !== undefined) {
-                let arenaObj = new ArenaObject({
-                    object_id: msgJSON.object_id,
-                    attributes: msgJSON.data,
-                    last_updated: msgJSON.timestamp
-                });
-                // TODO : add schema for pubsub message with catch on invalid format
-                switch (msgJSON.action) {
-                    case "create":
-                        await ArenaObject.find({ object_id: arenaObj.object_id }, (err, res)=>{
-                            if (res.length === 0) {
-                                arenaObj.createdAt = arenaObj.last_updated;
-                                arenaObj.save();
-                            } else {
-                                console.log("Already exists")
-                            }
-                        });
-                        break;
-                    case "update":
-                        let currentObj = await ArenaObject.findOne({ object_id: arenaObj.object_id });
-                        let dataUpdate = {};
-                        if (msgJSON.type === "merge") {
-                            dataUpdate = new Map([...currentObj.attributes, ...arenaObj.attributes])
+            let arenaObj = new ArenaObject({
+                object_id: msgJSON.object_id,
+                attributes: msgJSON.data,
+                last_updated: msgJSON.timestamp,
+                realm: topicSplit[0],
+                scene: topicSplit[2]
+            });
+            // TODO : add schema for pubsub message with catch on invalid format
+            switch (msgJSON.action) {
+                case "create":
+                    await ArenaObject.find({ object_id: arenaObj.object_id }, (err, res)=>{
+                        if (res.length === 0) {
+                            arenaObj.createdAt = arenaObj.last_updated;
+                            arenaObj.save();
                         } else {
-                            dataUpdate = arenaObj.attributes;
+                            console.log("Already exists")
                         }
-                        await ArenaObject.findOneAndUpdate(
-                            { object_id: arenaObj.object_id },
-                            { attributes: dataUpdate, last_updated: arenaObj.timestamp},
-                            {},
-                            (err, res) => {
-                                if (err) {
-                                    console.log("Doesn't exist!");
-                                }
-                            }
-                        );
-                        break;
-                    case "delete":
-                        await ArenaObject.deleteOne({ object_id: arenaObj.object_id }, (err, res) => {
+                    });
+                    break;
+                case "update":
+                    let currentObj = await ArenaObject.findOne({ object_id: arenaObj.object_id });
+                    let dataUpdate = {};
+                    if (msgJSON.type === "overwrite") {
+                        dataUpdate = arenaObj.attributes;
+                    } else {
+                        dataUpdate = new Map([...currentObj.attributes, ...arenaObj.attributes])
+                    }
+                    await ArenaObject.findOneAndUpdate(
+                        { object_id: arenaObj.object_id },
+                        { attributes: dataUpdate, last_updated: arenaObj.timestamp},
+                        {},
+                        (err, res) => {
                             if (err) {
-                                console.log("Doesn't exist or Already deleted")
+                                console.log("Doesn't exist!");
                             }
-                        });
-                        break;
-                    default:
-                        //pass
-                }
+                        }
+                    );
+                    break;
+                case "delete":
+                    await ArenaObject.deleteOne({ object_id: arenaObj.object_id }, (err, res) => {
+                        if (err) {
+                            console.log("Doesn't exist or Already deleted")
+                        }
+                    });
+                    break;
+                default:
+                    //pass
             }
         });
     } catch(e) {
