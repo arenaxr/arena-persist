767db799a42fe4db8b30fcc4dd745ee2d22df682
Ivan Liang <r33t@cmu.edu>
2024-07-26 01:40:13 -0400

    Update to latest core topics
    - None of these affect persist, which only needs public objs

--- topics.js
+++ topics.js
@@ -33,36 +33,34 @@ exports.TOPICS = Object.freeze({
     },
     SUBSCRIBE: {
         NETWORK:               '$NETWORK',
-        CHAT_PUBLIC:           `${REALM}/c/{nameSpace}/o/#`,
-        CHAT_PRIVATE:          `${REALM}/c/{nameSpace}/p/{idTag}/#`,
         DEVICE:                `${REALM}/d/{userName}/#`, // All client placeholder
         PROC_REG:              `${REALM}/proc/reg`,
         PROC_CTL:              `${REALM}/proc/control/{uuid}/#`,
         PROC_DBG:              `${REALM}/proc/debug/{uuid}`,
         SCENE_PUBLIC:          `${REALM}/s/{nameSpace}/{sceneName}/+/+`,
-        SCENE_PRIVATE:         `${REALM}/s/{nameSpace}/{sceneName}/+/+/{camName}/#`,
+        SCENE_PRIVATE:         `${REALM}/s/{nameSpace}/{sceneName}/+/+/{idTag}/#`,
     },
     PUBLISH: {
         NETWORK_LATENCY:       '$NETWORK/latency',
-        CHAT_PUBLIC:           `${REALM}/c/{nameSpace}/o/{idTag}`,
-        CHAT_PRIVATE:          `${REALM}/c/{nameSpace}/p/{toUid}/{idTag}`,
         DEVICE:                `${REALM}/d/{nameSpace}/{sceneName}/{idTag}`,
         PROC_REG:              `${REALM}/proc/reg`,
         PROC_CTL:              `${REALM}/proc/control`,
         PROC_DBG:              `${REALM}/proc/debug/{uuid}`,
         SCENE_PRESENCE:        `${REALM}/s/{nameSpace}/{sceneName}/x/{idTag}`,
+        SCENE_CHAT:            `${REALM}/s/{nameSpace}/{sceneName}/c/{idTag}`,
+        SCENE_CHAT_PRIVATE:    `${REALM}/s/{nameSpace}/{sceneName}/c/{idTag}/{toUid}`,
         SCENE_USER:            `${REALM}/s/{nameSpace}/{sceneName}/u/{userObj}`,
         SCENE_USER_PRIVATE:    `${REALM}/s/{nameSpace}/{sceneName}/u/{userObj}/{toUid}`, // Need to add face_ privs
         SCENE_OBJECTS:         `${REALM}/s/{nameSpace}/{sceneName}/o/{objectId}`, // All client placeholder
         SCENE_OBJECTS_PRIVATE: `${REALM}/s/{nameSpace}/{sceneName}/o/{objectId}/{toUid}`,
-        SCENE_RENDER:          `${REALM}/s/{nameSpace}/{sceneName}/r/{camName}`,
-        SCENE_RENDER_PRIVATE:  `${REALM}/s/{nameSpace}/{sceneName}/r/{camName}/-`, // To avoid unpriv sub
-        SCENE_ENV:             `${REALM}/s/{nameSpace}/{sceneName}/e/{camName}`,
-        SCENE_ENV_PRIVATE:     `${REALM}/s/{nameSpace}/{sceneName}/e/{camName}/-`, // To avoid unpriv sub
-        SCENE_PROGRAM:         `${REALM}/s/{nameSpace}/{sceneName}/p/{camName}`,
-        SCENE_PROGRAM_PRIVATE: `${REALM}/s/{nameSpace}/{sceneName}/p/{camName}/{toUid}`,
-        SCENE_DEBUG:           `${REALM}/s/{nameSpace}/{sceneName}/d/{camName}/-`, // To avoid unpriv sub
+        SCENE_RENDER:          `${REALM}/s/{nameSpace}/{sceneName}/r/{idTag}`,
+        SCENE_RENDER_PRIVATE:  `${REALM}/s/{nameSpace}/{sceneName}/r/{idTag}/-`, // To avoid unpriv sub
+        SCENE_ENV:             `${REALM}/s/{nameSpace}/{sceneName}/e/{idTag}`,
+        SCENE_ENV_PRIVATE:     `${REALM}/s/{nameSpace}/{sceneName}/e/{idTag}/-`, // To avoid unpriv sub
+        SCENE_PROGRAM:         `${REALM}/s/{nameSpace}/{sceneName}/p/{idTag}`,
+        SCENE_PROGRAM_PRIVATE: `${REALM}/s/{nameSpace}/{sceneName}/p/{idTag}/{toUid}`,
+        SCENE_DEBUG:           `${REALM}/s/{nameSpace}/{sceneName}/d/{idTag}/-`, // To avoid unpriv sub
     },
 });
 
-//export default TOPICS;
+// export default TOPICS;
